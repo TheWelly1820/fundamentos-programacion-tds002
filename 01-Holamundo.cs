f0f99ae3e6f8b282f4e9d09cee30f30f7375d38c
Wellington <20252554@itla.edu.do>
2026-09-15 18:36:26 -0400

    01: Hola Mundo y presentación
    02-reajuste

---
+++ 01-Holamundo.cs
@@ -0,0 +1,17 @@
+//aqui estare presentandome
+Console.WriteLine("|Hola me llamo wellington|");
+
+//Esta parte es solo para dejar un espacio entre la presentacion y la matricula
+
+Console.WriteLine("-------------------");
+
+//despues estare poniendo mi matricula
+Console.WriteLine("Mi matricula es |2025-2554|");
+//otro salto de linea 
+
+Console.WriteLine("------------------");
+
+//aqui dare una breve descripcion de mi persona
+Console.WriteLine("Soy una persona que le gusta aprender cosas nuevas y me gusta mucho la tecnologia");
+
+//ahora aqui voy a agregar un 
